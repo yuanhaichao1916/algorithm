c0487099dcd2082332c97c462d4061759f637b05
yuanhaichao <yhc_java@163.com>
2021-03-29 10:05:42 +0800

    Initial commit

---
+++ src/main/java/com/algorithm/Service/SumOfTwoNumbers.java
@@ -0,0 +1,54 @@
+package com.algorithm.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author Lenovo
+ * @Date 2021/3/29 9:42
+ */
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[]{2, 7, 11, 15}, 13));
+        System.out.println(twoSum2(new int[]{2, 7, 11, 15}, 13));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return new int[0];
+    }
+
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
